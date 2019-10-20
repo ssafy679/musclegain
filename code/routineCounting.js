5dfd8b885f88ea000ad68daa02655ed4c5947f0d
gahusb <bgg8988@naver.com>
2019-10-20 21:36:02 +0900

    training count

--- code/routineCounting.js
+++ code/routineCounting.js
@@ -1,25 +1,27 @@
-module.exports.function = function routineCounting (rname) {
+module.exports.function = function routineCounting(rName) {
 
-const console = require('console');
-const rData = require("./data/Routine.js");
+  const console = require('console');
+  const rData = require("./data/Routine.js");
 
-  if(rname == null){
+  if (rName == null) {
     console.log("routineName is null");
-  }else{
-     console.log("routineName : " + rname);
+  } else {
+    console.log("routineName : " + rName);
   }
 
-    /*------ 선언부 ------*/
+  /*------ 선언부 ------*/
   let results = [];
-  
+  var re = 3;
   /*------ 실행 ------*/
-  for(let i = 0; i < rData.length; i++) {
-    if(rData[i].rName.match(rname)) {
-      results.push(rData[i]);
-    }
-  }
+  // for(let i = 0; i < rData.length; i++) {
+  //   if(rData[i].rName.match(rName)) {
+  //     results.push(rData[i]);
+  //   }
+  // }
+  
+  return re;
 
-  console.log(results);
-  console.log("###RoutineCounting() end...");
-  return results;
+  // console.log(results);
+  // console.log("###RoutineCounting() end...");
+  // return results;
 }
